5a6960eb55706c3cb6f4f10f0970fccd4aac084e
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-22 18:57:40 -0600

    Fix width issue with app container on max pass length.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const characterCountSpan = document.getElementById("slider-character-count");
 const characterCountSlider = document.getElementById("slider-character");
+const passwordFieldEl = document.querySelector(".app-password-field");
+const generatePasswordButton = document.getElementById("generate-button");
 
 characterCountSlider.addEventListener("change", e => {
   if (e.target.value === "1") {
@@ -8,6 +10,15 @@ characterCountSlider.addEventListener("change", e => {
     characterCountSpan.textContent = `${e.target.value} characters`;
   }
 });
+
+generatePasswordButton.addEventListener("click", () => {
+  const password = generateNewPassword();
+  passwordFieldEl.textContent = password;
+});
+
+const generateNewPassword = () => {
+  return "ABC123456!@#!_&MOIKS";
+};
 
 window.addEventListener("load", () => {
   characterCountSpan.textContent = `${characterCountSlider.value} characters`;
