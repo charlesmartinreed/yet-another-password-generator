cbd6e9f203c1dc6d900ec32ebb529504eb905ef5
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-22 18:40:43 -0600

    Make slight styling changes.

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const characterCountSpan = document.getElementById("slider-character-count");
 const characterCountSlider = document.getElementById("slider-character");
 
 characterCountSlider.addEventListener("change", e => {
-  characterCountSpan.textContent = e.target.value;
-  characterCountSpan.classList.add("count-update");
+  if (e.target.value === "1") {
+    characterCountSpan.textContent = `${e.target.value} character`;
+  } else {
+    characterCountSpan.textContent = `${e.target.value} characters`;
+  }
 });
 
 window.addEventListener("load", () => {
-  characterCountSpan.textContent = `${characterCountSlider.value}`;
+  characterCountSpan.textContent = `${characterCountSlider.value} characters`;
 });
